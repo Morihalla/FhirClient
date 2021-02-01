01e603a9177e4754c0d130697ac7d8840be37c0d
Dieter <dieter.ketels@abrumet.be>
2021-02-01 23:08:34 +0100

    Changed to Generic (fluent) Client
    TODO: Load from text, create GUI

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,15 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.okhttp.client.OkHttpRestfulClientFactory;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.EncodingEnum;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.util.BundleUtil;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r5.model.AllergyIntolerance;
 import org.hl7.fhir.r5.model.Bundle;
+import org.hl7.fhir.r5.model.CodeableConcept;
+import org.hl7.fhir.r5.model.IdType;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,40 +19,59 @@ public class main {
 
     public static void main(String[] args) throws IOException {
 
-        // Create context for use of Ok Http + create client
+        // Create context once + create client
         FhirContext ctx = FhirContext.forR5();
         ctx.setRestfulClientFactory(new OkHttpRestfulClientFactory(ctx));
 
-        String serverBase = "https://hapi.fhir.org/baseR4";
+        String serverBase = "https://hapi.fhir.org/baseR5";
         IGenericClient client = ctx.newRestfulGenericClient(serverBase);
         client.setEncoding(EncodingEnum.XML);
         client.setPrettyPrint(true);
 
-        //Create list
-        List<IBaseResource> allergiesIntolerances = new ArrayList<>();
+        IParser parser = (IParser) ctx.newJsonParser();
+        parser.setPrettyPrint(true);
+
+        AllergyIntolerance allergyIntolerance = new AllergyIntolerance();
+        allergyIntolerance.setId("example");
+
+        MethodOutcome createOutcome = client.create()
+                .resource(allergyIntolerance)
+                .execute();
+
+        AllergyIntolerance readOutcome = client.read()
+                .resource(AllergyIntolerance.class)
+                .withId("1902")
+                .execute();
+
+        MethodOutcome updateOutcome = client.update()
+                .resource(allergyIntolerance.setId("new"))
+                .execute();
+
+        MethodOutcome deleteOutcome = client
+                .delete()
+                .resourceById(new IdType("AllergyIntolerance", "1506"))
+                .execute();
 
         // Search all IA's present
         Bundle bundle = client
                 .search()
                 .forResource(AllergyIntolerance.class)
+                .count(100)
                 .returnBundle(Bundle.class)
                 .execute();
-        allergiesIntolerances.addAll(BundleUtil.toListOfResources(ctx, bundle));
 
-        //Show result
-        System.out.println("Loaded " + allergiesIntolerances.size() + " Allergies/Intolerances!");
-        for (IBaseResource ai : allergiesIntolerances) {
-            System.out.println(ai.getIdElement().getValueAsString());
-        }
+        //Show results
+        System.out.println("Created: " + createOutcome.getId());
+        printDashedLine();
+        System.out.println("Read: " + readOutcome.getId());;
+        printDashedLine();
+        System.out.println("Updated: " + updateOutcome.getId());
+        printDashedLine();
+        System.out.println("Deleted: " + deleteOutcome.getId());
+        printDashedLine();
 
-        printDashedLine();
-        CRUDmethods.read();
-        printDashedLine();
-        CRUDmethods.delete();
-        printDashedLine();
-        CRUDmethods.create();
-        printDashedLine();
-        CRUDmethods.update();
+        System.out.println("Loaded " + bundle.getTotal() + " Allergies/Intolerances!");
+        System.out.println(bundle.getEntry().get(0).getResource().getId());
 
     }
 
